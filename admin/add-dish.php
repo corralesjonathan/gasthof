f289c093aaf5aff7a1485c722d2b4d64f2db33ab
corralesjonathan <jonathan.corralesruiz@ucr.ac.cr>
2023-10-25 20:35:05 -0600

    some admin.css  and administration pages changes

--- admin/add-dish.php
+++ admin/add-dish.php
@@ -1,18 +1,19 @@
 <?php 
     require_once '../database.php';
     // Reference: https://medoo.in/api/insert
-    
+    $featured = isset($_POST['featured']) ? $_POST['featured'] : 0;
+
     if ($_POST) {
     $dishData = [
         "dish_name" => $_POST["dish_name"],
         "dish_description"=> $_POST["dish_description"],
         "dish_image"=> $_POST["dish_image"],
         "dish_price"=> $_POST["dish_price"],
-        "featured"=> $_POST["featured"],
-        "people"=> $_POST["people"],
+        "featured" => $featured,
+        "id_dish_size" => $_POST["id_dish_size"], 
         "id_dish_category" => $_POST["id_dish_category"], 
     ];
-
+    
     $database->insert("tb_dishes", $dishData);
 }
 ?>
@@ -21,61 +22,71 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Add Dish</title>
+    <title>Gasthof-Add Dishes</title>
     <link rel="stylesheet" href="../css/themes/admin.css">
 </head>
 <body>
-    <div class="container">
-        <h2>Add New Dish</h2>
-        <form method="post" action="add-dish.php">
-            <div class="form-items">
-                <label for="dish_name">Dish name</label>
-                <input id="dish_name" class="textfield" name="dish_name" type="text">
-            </div>
-            <div class="form-items">
-                <label for="id_dish_category">Dish Category</label>
-                <select id="id_dish_category" name="id_dish_category">
-                    <option value="1">Starter</option>
-                    <option value="2">Main course</option>
-                    <option value="3">Dessert</option>
-                    <option value="4">Drink</option>
-                </select>
+    <div class="main-container">
+    <img src="../imgs/gasthof-logo.webp" alt="Gathof Logo">
+    <h2>Add New Dish</h2>
+        <div class="container">
+
+            <div class="column">
+                <form method="post" action="add-dish.php">
+                    <div class="form-items">
+                        <label for="dish_name">Dish name</label>
+                        <input id="dish_name" class="textfield" name="dish_name" type="text">
+                    </div>
+                    <div class="form-items">
+                        <label for="dish_description">Dish Description</label>
+                        <textarea id="dish_description" name="dish_description" cols="30" rows="10"></textarea>
+                    </div>
+                    <div class="form-items">
+                        <label for="dish_price">Dish Price</label>
+                        <input id="dish_price" class="textfield" name="dish_price" type="text">
+                    </div>
             </div>
 
-            <div class="form-items">
-                <label for="dish_description">Dish Description</label>
-                <textarea id="dish_description" name="dish_description" cols="30" rows="10"></textarea>
+            <div class="column">
+                    <div class="form-items">
+                        <label for="id_dish_category">Dish Category</label>
+                        <select id="id_dish_category" name="id_dish_category">
+                            <option value="1">Starter</option>
+                            <option value="2">Main course</option>
+                            <option value="3">Dessert</option>
+                            <option value="4">Drink</option>
+                        </select>
+                    </div>
+                    <div class="form-items">
+                        <label for="id_dish_size">Dish Size</label>
+                        <select id="id_dish_size" name="id_dish_size">
+                            <option value="1">Individual</option>
+                            <option value="2">Couples</option>
+                            <option value="3">Family</option>
+                        </select>
+                    </div>
+                    <div class="form-items">
+                        <div>
+                        <label>Is a freatured dish?</label>
+                        <input id="featured" type="radio" name="featured" value="1"> Yes
+                        <input id="featured" type="radio" name="featured" value="0"> No
+                        </div>
+                    </div>
             </div>
-            <div class="form-items">
-                <label for="dish_image">Dish Image</label>
-                <img id="preview" src="../imgs/placeholder-img.jpg" alt="Preview">
-                <input id="dish_image" type="file" name="dish_image" onchange="readURL(this)">
-            </div>
-            <div class="form-items">
-                <label for="dish_price">Dish Price</label>
-                <input placeholder="$" id="dish_price" class="textfield" name="dish_price" type="text">
-            </div>
-            <div class="form-items">
-                <label for="featured">Is the dish featured?</label>
-                <select id="featured" name="featured">
-                    <option value="1">Yes</option>
-                    <option value="0">No</option>
-                </select>
-            </div>
-            <div class="form-items">
-                <label for="people">This dish is:</label>
-                <select id="people" name="people">
-                    <option value="Individual">Individual</option>
-                    <option value="Couples">Couples</option>
-                    <option value="Family">Famiy</option>
-                </select>
-            </div>
-            <div class="form-items">
-                <input class="submit-btn" type="submit" value="Add New Dish">
-            </div>
-        </form>
+
+            <div class="column">
+                    <div class="form-items">
+                        <label for="dish_image">Dish Image</label>
+                        <img id="preview" src="../imgs/placeholder-img.jpg" alt="Preview">
+                        <input id="dish_image" type="file" name="dish_image" onchange="readURL(this)">
+                    </div>
+                        <div class="form-items">
+                        <input class="submit-btn" type="submit" value="Add New Dish">
+                    </div>
+            </div>   
+            </form>
+        </div>
     </div>
-
     <script>
         function readURL(input) {
             if(input.files && input.files[0]){
--- admin/add-users.php
+++ admin/add-users.php
@@ -22,39 +22,40 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Gasthof-Add Users</title>
     <link rel="stylesheet" href="../css/themes/admin.css">
 </head>
 <body>
-    
-<div class="container">
-        <h2>Add New User</h2>
-        <form method="post" action="add-users.php">
-            <div class="form-items">
-                <label for="name">Name</label>
-                <input id="name" class="textfield" name="name" type="text">
-            </div>
-            <div class="form-items">
-                <label for="lastname">Last name</label>
-                <input id="lastname" class="textfield" name="lastname" type="text">
-            </div>
-            <div class="form-items">
-                <label for="usr">Username</label>
-                <input id="usr" class="textfield" name="usr" type="text">
-            </div>
-            <div class="form-items">
-                <label for="email">Email</label>
-                <input id="email" class="textfield" name="email" type="text">
-            </div>
-            <div class="form-items">
-                <label for="pwd">Password</label>
-                <input id="pwd" class="textfield" name="pwd" type="password">
-            </div>
-            <div class="form-items">
-                <input class="submit-btn" type="submit" value="Add new user">
-            </div>
-        </form>
-    </div>
-    
+<div class="main-container">
+    <img src="../imgs/gasthof-logo.webp" alt="Gathof Logo">
+    <h2>Add New User</h2>
+    <div class="container">   
+            <form method="post" action="add-users.php">
+                <div class="form-items">
+                    <label for="name">Name</label>
+                    <input id="name" class="textfield" name="name" type="text">
+                </div>
+                <div class="form-items">
+                    <label for="lastname">Last name</label>
+                    <input id="lastname" class="textfield" name="lastname" type="text">
+                </div>
+                <div class="form-items">
+                    <label for="usr">Username</label>
+                    <input id="usr" class="textfield" name="usr" type="text">
+                </div>
+                <div class="form-items">
+                    <label for="email">Email</label>
+                    <input id="email" class="textfield" name="email" type="text">
+                </div>
+                <div class="form-items">
+                    <label for="pwd">Password</label>
+                    <input id="pwd" class="textfield" name="pwd" type="password">
+                </div>
+                <div class="form-items">
+                    <input class="submit-btn" type="submit" value="Add new user">
+                </div>
+            </form>
+        </div>
+</div>    
 </body>
 </html>
